9d259e4aed6a71dc146facbd72b3d9338282f537
Kevin Pérez <kevinpt2@hotmail.com>
2022-03-02 18:32:10 -0600

    Update index.php

--- index.php
+++ index.php
@@ -53,9 +53,9 @@
         platforms = this.physics.add.staticGroup();
         platforms.create(400,568, 'ground'),setScale(2).refreshBody();
 
-        platforms.create(500,300, 'ground');
-        platforms.create(100,200, 'ground');
-        platforms.create(600,100, 'ground');
+        platforms.create(600, 400, 'ground');
+        platforms.create(50, 250, 'ground');
+        platforms.create(750, 220, 'ground');
         
     }
 
